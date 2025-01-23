a9474320aa714f02f631208db51cb7c7ad9cedea
Sergio <sergio.ruedagonzalez@riberadeltajo.es>
2025-01-23 09:56:18 +0100

    primeras migraciones y seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
+    use SoftDeletes;//implementa un borrado lógico
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -32,7 +35,13 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
+    /**
+     * Get the tasks for the user.
+     */
+    public function task(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
     /**
      * Get the attributes that should be cast.
      *
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();//controla si el dato esta borrado o no
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
